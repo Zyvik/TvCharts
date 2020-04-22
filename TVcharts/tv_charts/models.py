92460ec06167c94365e28f3a4b8a888f2aac9084
zyvik <pawel86gw2@gmail.com>
2020-04-22 17:36:50 +0200

    Created django models (TvSeries, Episodes)

--- TVcharts/tv_charts/models.py
+++ TVcharts/tv_charts/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+class TvSeries(models.Model):
+    title = models.CharField(max_length=50)
+    original_title = models.CharField(max_length=50, default=None)
+    imdb_url = models.CharField(max_length=100)
+    poster_url = models.CharField(max_length=100)
+    rating = models.DecimalField(decimal_places=1, max_digits=3)
+    votes = models.IntegerField()
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class Episodes(models.Model):
+    series = models.ForeignKey(TvSeries, models.CASCADE)
+    season = models.IntegerField()
+    episode_nr = models.IntegerField()
+    title = models.CharField(max_length=50)
+    thumbnail = models.CharField(max_length=100)
+    rating = models.DecimalField(decimal_places=1, max_digits=3)
+    votes = models.IntegerField()
+    air_date = models.CharField(max_length=50)  # change it to DateField after creating conversion code
+
+    class Meta:
+        ordering = ['series', 'season', 'episode_nr']
+
+    def __str__(self):
+        return f'{self.series} s{self.season}e{self.episode_nr}'
---
+++ TVcharts/tv_charts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+app_name = 'tv_charts'
+urlpatterns = [
+]
